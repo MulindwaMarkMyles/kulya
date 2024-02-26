8d00ba8b2f0d96bde56a2fe3b0a6cadb876110ba
Mulindwa Myles Mark <mylesmark752@gmail.com>
2024-02-26 18:01:46 +0300

    doing models

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        city = models.CharField(max_length=100)
+        village = models.CharField(max_length=100)
+        phone_number = models.IntegerField()
+        
+class 
