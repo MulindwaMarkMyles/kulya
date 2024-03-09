d516a6bc3ab8bc5a189db86c84eb998be73abfc7
Mulindwa Myles Mark <mylesmark752@gmail.com>
2024-03-09 22:18:41 +0300

    finished the add to cart logic and cart logic

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
         path('cart/', cart, name="cart"),
         path('checkout/', checkout, name="checkout"),
         path('update-item/', updateitem, name="update-item"),
+        path('process-order/', processOrder, name="process-order"),
 ]
