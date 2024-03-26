22823bdb4e4d2acae2ddf71e7baf02fc05f0984b
Mulindwa Myles Mark <mylesmark752@gmail.com>
2024-03-26 23:27:03 +0300

    configuring tests

--- tests/factories.py
+++ tests/factories.py
@@ -1,16 +1,26 @@
 import factory
 from faker import Faker
+from authentication.models import Customer, Business, Profile
+from shop.models import Product, Category, Order, OrderItem, ShippingAddress
+from django.contrib.auth.models import User
 
 fake = Faker()
 
-from authentication.models import Customer, Business
-from shop.models import Product, Category
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        
+    username = fake.name()
+    email = fake.email()
+    password = fake.name()
 
 class CustomerFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Customer
-
+    	
+    user = factory.SubFactory(UserFactory)
     first_name = fake.name()
+    last_name = fake.name()
     email = fake.email()
     
 class CategoryFactory(factory.django.DjangoModelFactory):
@@ -23,9 +33,19 @@ class BusinessFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Business
         
+    owner = factory.SubFactory(UserFactory)
     first_name = fake.name()
+    last_name = fake.name()
+    business_name = fake.text()
     email = fake.email()
     
+class ProfileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Profile
+    
+    user = factory.SubFactory(UserFactory)
+    image = fake.image_url()
+        
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Product
@@ -38,4 +58,32 @@ class ProductFactory(factory.django.DjangoModelFactory):
     category = factory.SubFactory(CategoryFactory)
     image = fake.image_url()
     rating = 5.0
+    
+class OrderFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Order
+        
+    customer = factory.SubFactory(UserFactory)
+    complete = False
+    transaction_id = fake.text()
+
+class OrderItemFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = OrderItem
+        
+    # product = factory.SubFactory(ProductFactory)
+    # order = factory.SubFactory(OrderFactory)
+    quantity = fake.random_number(10)
+    
+class AddressFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ShippingAddress
+        
+    # customer = factory.SubFactory(UserFactory)
+    # order = factory.SubFactory(OrderFactory)
+    address = fake.text()
+    city = fake.text()
+    state = fake.text()
+    zipcode = fake.text()
+    
     
--- tests/test_2.py
+++ tests/test_2.py
@@ -3,19 +3,19 @@ from faker import Faker
 
 fake = Faker()
 
-@pytest.mark.parametrize(
-    "name, description, price, digital, image, rating",
-    [
-        (fake.name(), fake.text(), "10000.00", fake.boolean(),fake.image_url(), 5.0),
-        (fake.name(), fake.text(), "810000.00", fake.boolean(),fake.image_url(), 10.0),
-        (fake.name(), fake.text(), "840000.00", fake.boolean(),fake.image_url(), 14.0),
-        (fake.name(), fake.text(), "90000.00", fake.boolean(),fake.image_url(), 18.0),
-    ],
-)
-def test_product(db, product_factory, name, description, price, digital, image, rating):
-    product = product_factory(name=name, description=description, price=price, digital=digital, image=image, rating=rating)
-    assert product.name == name
-    assert product.description == description
-    assert product.price == price
-    assert product.digital == digital
-    assert product.rating == rating
+# @pytest.mark.parametrize(
+#     "name, description, price, digital, image, rating",
+#     [
+#         (fake.name(), fake.text(), "10000.00", fake.boolean(),fake.image_url(), 5.0),
+#         (fake.name(), fake.text(), "810000.00", fake.boolean(),fake.image_url(), 10.0),
+#         (fake.name(), fake.text(), "840000.00", fake.boolean(),fake.image_url(), 14.0),
+#         (fake.name(), fake.text(), "90000.00", fake.boolean(),fake.image_url(), 18.0),
+#     ],
+# )
+# def test_product(db, product_factory, name, description, price, digital, image, rating):
+#     product = product_factory(name=name, description=description, price=price, digital=digital, image=image, rating=rating)
+#     assert product.name == name
+#     assert product.description == description
+#     assert product.price == price
+#     assert product.digital == digital
+#     assert product.rating == rating
