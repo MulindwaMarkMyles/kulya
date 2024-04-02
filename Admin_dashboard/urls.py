0f3216678f6464b6e33b4f6289626c562bd38a8b
Naluwujja Ashley Hope <ashleyhope2345@gmail.com>
2024-04-02 22:13:30 +0300

    admin login

--- Admin_dashboard/urls.py
+++ Admin_dashboard/urls.py
@@ -1,7 +1,9 @@
 from .views import *
 from django.urls import path
+from Admin_dashboard import admin as custom_admin
 
 
 urlpatterns = [
-    path("",index, name="index")
+    path("",index, name="index"),
+    path('admin/', custom_admin.admin_site.urls),
 ]
