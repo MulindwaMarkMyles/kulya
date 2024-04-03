3653ff8b4a93511a53f8b171f0dab7d32bec927a
Mulindwa Myles Mark <mylesmark752@gmail.com>
2024-04-03 07:06:00 +0300

    made changes to home page banner

--- tests/authentication/test_models.py
+++ tests/authentication/test_models.py
@@ -11,13 +11,29 @@ class TestProfileModel:
         
     def  test_imageurl_return(self, profile_factory):
         image_path = faker.Faker().image_url()
-        profile = profile_factory(image=image_path)
-        assert profile.imageurl != image_path
+        profile_one = profile_factory(image=image_path)
+        profile_two = profile_factory(image="None")
+        
+        assert profile_two.imageurl == ""
+        assert profile_one.imageurl != image_path
         
     def test_save_method(self, profile_factory):
         image = faker.Faker().image_url().replace("https://","")
         image = image.replace("/","") + ".jpg"
         profile = profile_factory(image=image)
-        print(image)
-        print(profile.image.name)
         assert profile.image.name != image
+        
+class TestCustomerModel:
+    def test_name_return(self, customer_factory):
+        customer = customer_factory(first_name="Sulaiman", last_name="Ashley")
+        assert customer.name == "Sulaiman Ashley"
+    
+    def test_str_return(self, customer_factory):
+        customer = customer_factory(first_name="Sulaiman", last_name="Ashley")
+        assert customer.__str__() == "Sulaiman Ashley"
+        
+class TestBusinessModel:
+    def test_str_return(self, business_factory):
+        company = faker.Faker().company()
+        business = business_factory(business_name=company)
+        assert business.__str__() == company
