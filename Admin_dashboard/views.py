87ea48004029706dfde8280715fc0aa549d7cdd0
blehhh <ankunda.praise@students.mak.ac.ug>
2024-04-13 23:15:27 +0300

    Changed placement of terms and conditions url.

--- Admin_dashboard/views.py
+++ Admin_dashboard/views.py
@@ -14,7 +14,7 @@ def business_list(request):
 
 def business_detail(request, pk):
     business = get_object_or_404(Business, pk=pk)
-    return render(request, 'business_detail.html', {'business': business})
+    return render(request, 'Admin_dashboard/business_list.html', {'business': business})
 
 def business_create(request):
     if request.method == 'POST':
@@ -39,7 +39,7 @@ def customer_list(request):
 
 def customer_detail(request, pk):
     customer = get_object_or_404(Customer, pk=pk)
-    return render(request, 'customer_detail.html', {'customer': customer})
+    return render(request, 'Admin_dashboard/customer_detail.html', {'customer': customer})
 
 def customer_create(request):
     if request.method == 'POST':
@@ -80,6 +80,59 @@ def customer_delete(request, pk):
     customers = get_object_or_404(Customer, pk=pk)
     if request.method == 'POST':
         customers.delete()
+        return redirect('profile_list')
+    return render(request, 'Admin_dashboard/customer_delete.html', {'customers': customers})
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    return render(request, "Admin_dashboard/customer_list.html", {'customers': customers})
+
+def customer_detail(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    return render(request, 'customer_detail.html', {'customer': customer})
+
+def customer_create(request):
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('customer_list')
+    else:
+        form = CustomerForm()
+    return render(request, 'Admin_dashboard/customer_create.html', {'form': form})
+
+def customer_delete(request, pk):
+    customers = get_object_or_404(Customer, pk=pk)
+    if request.method == 'POST':
+        customers.delete()
         return redirect('customer_list')
     return render(request, 'Admin_dashboard/customer_delete.html', {'customers': customers})
 
+def profile_list(request):
+    profiles = Profile.objects.all()
+    return render(request, "Admin_dashboard/profile_list.html", {'profiles': profiles})
+
+def profile_detail(request, pk):
+    profile = get_object_or_404(Profile, pk=pk)
+    return render(request, 'profile_detail.html', {'profile': profile})
+
+def profile_create(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('profile_list')
+    else:
+        form = ProfileForm()
+    return render(request, 'Admin_dashboard/profile_create.html', {'form': form})
+
+def profile_delete(request, pk):
+    profiles = get_object_or_404(Profile, pk=pk)
+    if request.method == 'POST':
+        profiles.delete()
+        return redirect('profile_list')
+    return render(request, 'Admin_dashboard/profile_delete.html', {'profiles': profiles})
+
+def view_owner(request):
+    business = Business.objects.all()
+    return render(request, 'Admin_dashboard/view_owner.html')
